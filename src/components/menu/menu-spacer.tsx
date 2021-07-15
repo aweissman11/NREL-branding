8b39a20c8823e8a7989500b09b23a0de13de21f7
Aaron <aweissma@nrel.gov>
2021-07-15 10:41:08 -0600

    Create component package
    - Add components and convert to TypeScript
    - Install relevant packages
    - Set up bundling
    - Write README
    
    Package is now ready for publishing

---
+++ src/components/menu/menu-spacer.tsx
@@ -0,0 +1,14 @@
+import './style.scss';
+
+/**
+ * Adds space between menu items.
+ *
+ * @param {object} props
+ */
+function MenuSpacer() {
+  return (
+    <li className="menu-spacer"></li>
+  );
+}
+
+export default MenuSpacer;
